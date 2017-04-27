00b872e9aeaade9a085899118537df8f25789aff
Carlos <carl7755@easv365.dk>
2017-04-27 14:17:40 +0200

    Manager Login View

---
+++ src/museumApp/gui/controller/ManagerLoginViewController.java
@@ -0,0 +1,29 @@
+/*
+ * Erhvervsakademi Sydvest, Computer Science 2016-2017, Carlos F. Ognissanti
+ * To change this header, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package museumApp.gui.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Yuki
+ */
+public class ManagerLoginViewController implements Initializable
+  {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb)
+      {
+        // TODO
+      }
+
+  }
