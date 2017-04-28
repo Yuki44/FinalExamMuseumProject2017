1ccae1c09ce20ee96c48f46ce650426f78cb688d
Carlos <carl7755@easv365.dk>
2017-04-28 14:05:59 +0200

    Deleted some views

--- src/museumApp/gui/controller/VolunteerRegisterHoursViewController.java
+++ src/museumApp/gui/controller/VolunteerRegisterHoursViewController.java
@@ -5,14 +5,19 @@
  */
 package museumApp.gui.controller;
 
+import com.jfoenix.controls.JFXTextField;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
@@ -27,6 +32,8 @@ public class VolunteerRegisterHoursViewController extends Controller implements 
 
     @FXML
     private BorderPane borderPane;
+    @FXML
+    private JFXTextField setHoursLabel;
 
     /**
      * Initializes the controller class.
@@ -67,24 +74,57 @@ public class VolunteerRegisterHoursViewController extends Controller implements 
     @FXML
     private void goToFinalView(MouseEvent event)
       {
-        try
+        /**
+         * If the hours label is not empty then show an alert displaying how many
+         * hours we input,
+         * You can cancel or say "ok", if you say ok it displays a second alert
+         * thanking you for your effort, and then goes to the main view.
+         */
+        if (!setHoursLabel.getText().isEmpty())
         {
-            Stage stage;
-            Parent root;
-            stage = (Stage) borderPane.getScene().getWindow();
-            URL location = this.getClass().getResource("/museumApp/gui/view/LessThanStandardHours.fxml");
-            System.out.println(location.getPath());
-            FXMLLoader loader = new FXMLLoader(location);
-            root = loader.load();
-            Scene scene = new Scene(root);
-            stage.hide();
-            stage.setScene(scene);
-            stage.show();
-            stage.centerOnScreen();
-        }
-        catch (IOException ex)
-        {
-            System.err.println(ex);
+            Alert alert = new Alert(AlertType.CONFIRMATION);
+            alert.setTitle("\nRingkøbing-Skjern Museum");
+            alert.setHeaderText("You set " + setHoursLabel.getText().trim() + " hours.");
+            alert.setContentText("\nAre you sure about this amount of hours?");
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.get() == ButtonType.OK)
+            {
+                Alert congratsAlert = new Alert(AlertType.INFORMATION);
+                congratsAlert.setTitle("\nRingkøbing-Skjern Museum");
+                congratsAlert.setHeaderText("Thank you for your contribution to Ringkøbing-skjern museum!");
+                congratsAlert.setContentText("\nHave a nice day!!");
+
+                Optional<ButtonType> congratsResult = congratsAlert.showAndWait();
+                if (congratsResult.get() == ButtonType.OK)
+                {
+                    try
+                    {
+                        Stage stage;
+                        Parent root;
+                        stage = (Stage) borderPane.getScene().getWindow();
+                        URL location = this.getClass().getResource("/museumApp/gui/view/LanguageSelectionView.fxml");
+                        System.out.println(location.getPath());
+                        FXMLLoader loader = new FXMLLoader(location);
+                        root = loader.load();
+                        Scene scene = new Scene(root);
+                        stage.hide();
+                        stage.setScene(scene);
+                        stage.show();
+                        stage.centerOnScreen();
+                    }
+                    catch (IOException ex)
+                    {
+                        System.err.println(ex);
+                    }
+                }
+                else
+                {
+                }
+            }
+            else
+            {
+                alert.close(); //If you press cancel on the first alert then it closes
+            }
         }
       }
 
