a54ede2f772f49740aadb2319d7c595de7975c49
LindaBraarup <linda@bjskysealand.com>
2017-05-31 14:25:00 +0200

    update volunteer_time, guild, employee

--- src/museumApp/dal/UpdateData.java
+++ src/museumApp/dal/UpdateData.java
@@ -32,9 +32,22 @@ public class UpdateData extends DatabaseManager
      */
     public void updateAdministrator(Administrator ad) throws SQLException
       {
-        // TODO
+         String sql = "UPDATE employee SET e_first_name=? e_last_name=? e_email=? user_name=? password=?"
+                   + " WHERE employee_id=?";
+        try (Connection con = connectionManager.getConnection())
+        {
+            PreparedStatement pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, ad.getFirstNameAsString());
+            pstmt.setString(2, ad.getLastNameAsString());
+            pstmt.setString(3,ad.getEmailAsString());
+            pstmt.setString(4, ad.getUserNameAsString());
+            pstmt.setString(5, ad.getPasswordAsString());
+            pstmt.setInt(6, ad.getId());
+           
+             pstmt.execute();
       }
-
+      }
+    
     /** ------------------------------------------------------------------------------------------------------------------------------------------------------. */
     /**
      *
@@ -42,10 +55,18 @@ public class UpdateData extends DatabaseManager
      * @throws SQLException
      */
     public void updateGuild(Guild gd) throws SQLException
-      {
-        // TODO
+       {
+           String sql = "UPDATE guild SET name=? ,manager_id=? WHERE guild_id=?";
+        try (Connection con = connectionManager.getConnection())
+        {
+            PreparedStatement pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, gd.getNameAsString());
+            pstmt.setInt(2, gd.getManager().getId());
+            pstmt.setInt(3, gd.getId());
+            
+             pstmt.execute();
       }
-
+      }
     /** ------------------------------------------------------------------------------------------------------------------------------------------------------. */
     /**
      *
@@ -65,7 +86,20 @@ public class UpdateData extends DatabaseManager
      */
     public void updateManager(Manager mg) throws SQLException
       {
-        // TODO
+         String sql = "UPDATE employee SET e_first_name=? e_last_name=? e_email=? user_name=? password=?"
+                   + " WHERE employee_id=?";
+        try (Connection con = connectionManager.getConnection())
+        {
+            PreparedStatement pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, mg.getFirstNameAsString());
+            pstmt.setString(2, mg.getLastNameAsString());
+            pstmt.setString(3,mg.getEmailAsString());
+            pstmt.setString(4, mg.getUserNameAsString());
+            pstmt.setString(5, mg.getPasswordAsString());
+            pstmt.setInt(6, mg.getId());
+           
+             pstmt.execute();
+      }
       }
 
     /** ------------------------------------------------------------------------------------------------------------------------------------------------------. */
@@ -76,9 +110,18 @@ public class UpdateData extends DatabaseManager
      */
     public void updateVolunteerTime(VolunteerTime vTime) throws SQLException
       {
-        // TODO
+           String sql = "UPDATE volunteer_time SET hours=? WHERE guild_id=? AND volunteer_id=? AND date=? ";
+        try (Connection con = connectionManager.getConnection())
+        {
+            PreparedStatement pstmt = con.prepareStatement(sql);
+            pstmt.setInt(1, vTime.getHours());
+            pstmt.setInt(2, vTime.getGuild().getId());
+            pstmt.setInt(3, vTime.getVolunteer().getId());
+            pstmt.setDate(4,vTime.getDate());
+            
+             pstmt.execute();
       }
-
+      }
     /** ------------------------------------------------------------------------------------------------------------------------------------------------------. */
     /**
      *
