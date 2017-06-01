07ebf20bec9e19ba9318abb43190c4acb70485d9
PBLauge <peder.laugesen@gmail.com>
2017-06-01 12:29:13 +0200

    JavaDoc in DAL done :coffee:

--- src/museumApp/dal/UpdateData.java
+++ src/museumApp/dal/UpdateData.java
@@ -26,28 +26,29 @@ public class UpdateData extends DatabaseManager
     /** ------------------------------------------------------------------------------------------------------------------------------------------------------. */
     /** ------------------------------------------------------------------------------------------------------------------------------------------------------. */
     /**
+     * This method creates the update query in the database.
      *
      * @param ad
      * @throws SQLException
      */
     public void updateAdministrator(Administrator ad) throws SQLException
       {
-         String sql = "UPDATE employee SET e_first_name=?, e_last_name=?, e_email=?, user_name=?, password=?"
-                   + " WHERE employee_id=?";
+        String sql = "UPDATE employee SET e_first_name=?, e_last_name=?, e_email=?, user_name=?, password=?"
+                + " WHERE employee_id=?";
         try (Connection con = connectionManager.getConnection())
         {
             PreparedStatement pstmt = con.prepareStatement(sql);
             pstmt.setString(1, ad.getFirstNameAsString());
             pstmt.setString(2, ad.getLastNameAsString());
-            pstmt.setString(3,ad.getEmailAsString());
+            pstmt.setString(3, ad.getEmailAsString());
             pstmt.setString(4, ad.getUserNameAsString());
             pstmt.setString(5, ad.getPasswordAsString());
             pstmt.setInt(6, ad.getId());
-           
-             pstmt.execute();
+
+            pstmt.execute();
+        }
       }
-      }
-    
+
     /** ------------------------------------------------------------------------------------------------------------------------------------------------------. */
     /**
      *
@@ -55,18 +56,19 @@ public class UpdateData extends DatabaseManager
      * @throws SQLException
      */
     public void updateGuild(Guild gd) throws SQLException
-       {
-           String sql = "UPDATE guild SET name=? ,manager_id=? WHERE guild_id=?";
+      {
+        String sql = "UPDATE guild SET name=? ,manager_id=? WHERE guild_id=?";
         try (Connection con = connectionManager.getConnection())
         {
             PreparedStatement pstmt = con.prepareStatement(sql);
             pstmt.setString(1, gd.getNameAsString());
             pstmt.setInt(2, gd.getManager().getId());
             pstmt.setInt(3, gd.getId());
-            
-             pstmt.execute();
+
+            pstmt.execute();
+        }
       }
-      }
+
     /** ------------------------------------------------------------------------------------------------------------------------------------------------------. */
     /**
      *
@@ -86,20 +88,20 @@ public class UpdateData extends DatabaseManager
      */
     public void updateManager(Manager mg) throws SQLException
       {
-         String sql = "UPDATE employee SET e_first_name=? ,e_last_name=? ,e_email=? ,user_name=? ,password=?"
-                   + " WHERE employee_id=?";
+        String sql = "UPDATE employee SET e_first_name=? ,e_last_name=? ,e_email=? ,user_name=? ,password=?"
+                + " WHERE employee_id=?";
         try (Connection con = connectionManager.getConnection())
         {
             PreparedStatement pstmt = con.prepareStatement(sql);
             pstmt.setString(1, mg.getFirstNameAsString());
             pstmt.setString(2, mg.getLastNameAsString());
-            pstmt.setString(3,mg.getEmailAsString());
+            pstmt.setString(3, mg.getEmailAsString());
             pstmt.setString(4, mg.getUserNameAsString());
             pstmt.setString(5, mg.getPasswordAsString());
             pstmt.setInt(6, mg.getId());
-           
-             pstmt.execute();
-      }
+
+            pstmt.execute();
+        }
       }
 
     /** ------------------------------------------------------------------------------------------------------------------------------------------------------. */
@@ -110,18 +112,19 @@ public class UpdateData extends DatabaseManager
      */
     public void updateVolunteerTime(VolunteerTime vTime) throws SQLException
       {
-           String sql = "UPDATE volunteer_time SET hours=? WHERE guild_id=? AND volunteer_id=? AND date=? ";
+        String sql = "UPDATE volunteer_time SET hours=? WHERE guild_id=? AND volunteer_id=? AND date=? ";
         try (Connection con = connectionManager.getConnection())
         {
             PreparedStatement pstmt = con.prepareStatement(sql);
             pstmt.setInt(1, vTime.getHours());
             pstmt.setInt(2, vTime.getGuild().getId());
             pstmt.setInt(3, vTime.getVolunteer().getId());
-            pstmt.setDate(4,vTime.getDate());
-            
-             pstmt.execute();
+            pstmt.setDate(4, vTime.getDate());
+
+            pstmt.execute();
+        }
       }
-      }
+
     /** ------------------------------------------------------------------------------------------------------------------------------------------------------. */
     /**
      *
@@ -136,21 +139,21 @@ public class UpdateData extends DatabaseManager
         try (Connection con = connectionManager.getConnection())
         {
             PreparedStatement pstmt = con.prepareStatement(sql);
-            pstmt.setString(1,vtr.getFirstNameAsString());
+            pstmt.setString(1, vtr.getFirstNameAsString());
             pstmt.setString(2, vtr.getLastNameAsString());
-            pstmt.setString(3,vtr.getBirthDateAString());
+            pstmt.setString(3, vtr.getBirthDateAString());
             pstmt.setString(4, vtr.getPhoneNumberAsString());
             pstmt.setString(5, vtr.getNationalityAsString());
             pstmt.setString(6, vtr.getEmailAsString());
-            pstmt.setDate(7,vtr.getRegisteredDate());
+            pstmt.setDate(7, vtr.getRegisteredDate());
             pstmt.setString(8, vtr.getCommentAsString());
             pstmt.setString(9, vtr.getAddressAsString());
             pstmt.setString(10, vtr.getCityAsString());
             pstmt.setString(11, vtr.getZipCodeAsString());
             pstmt.setString(12, vtr.getCountryAsString());
-            pstmt.setString(13,vtr.getPhotoAsString());
+            pstmt.setString(13, vtr.getPhotoAsString());
             pstmt.setInt(14, vtr.getId());
-            
+
             pstmt.execute();
         }
       }
