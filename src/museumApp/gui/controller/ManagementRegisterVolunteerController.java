f3ef3015002179d0ff6ff4b220104e7dc7ea7b8a
PBLauge <peder.laugesen@gmail.com>
2017-06-01 08:41:01 +0200

    Finish some of the TODO's :coffee:

--- src/museumApp/gui/controller/ManagementRegisterVolunteerController.java
+++ src/museumApp/gui/controller/ManagementRegisterVolunteerController.java
@@ -237,7 +237,9 @@ public class ManagementRegisterVolunteerController extends Controller implements
     private ComboBox<Guild> comboBoxGuildSelection;
     @FXML
     private ComboBox<Guild> comboBoxSelectGuildCsv;
+    @FXML
     private Label lblPrintGdHoursSuccess;
+    @FXML
     private Label lblPrintAllVtrSuccess;
     @FXML
     private GridPane gridPaneSuccessMsg;
